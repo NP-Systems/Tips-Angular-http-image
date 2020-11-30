c83707130c1116c001f42590dfebf4bcde973ef7
awesome.rainbows7@gmail.com <awesome.rainbows7@gmail.com>
2020-11-30 18:20:23 +0900

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+import io
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from flask import Flask, send_file
 from flask_cors import CORS
 import json
 
@@ -8,8 +13,15 @@ CORS(app)
 
 @app.route('/',methods=["GET","POST"])
 def hello():
-    name = "Hello World"
-    return json.dumps({'name':name})
+    image = io.BytesIO()
+    x = np.linspace(0, 10)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.savefig(image, format='png')
+    image.seek(0)
+    return send_file(image,
+                     attachment_filename="image.png",
+                     as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',debug=True)
--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  unsafeImageUrl:any;
+  imageUrl:any;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private sanitizer:DomSanitizer)
+  {
     this.hello();
   }
 
   hello(){
-    this.http.get('http://127.0.0.1:5000')
+    this.http.get('http://127.0.0.1:5000',{responseType:'blob'})
      .subscribe(
-       (res) => {
+       (res:any) => {
          console.log(res);
+         this.unsafeImageUrl = URL.createObjectURL(res);
+         this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(this.unsafeImageUrl);
        })
   }
 
